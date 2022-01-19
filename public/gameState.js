06d2b8ce563cb98631d9ced911b0353435b6c29d
Chris Porter <chris.porter@uplight.com>
2022-01-18 22:22:36 -0800

    Add initial game state class

---
+++ public/gameState.js
@@ -0,0 +1,21 @@
+export class GameState {
+  constructor(target) {
+    this.target = target
+    this.guesses = [];
+    this.keyboard = new Keyboard();
+  }
+}
+
+export class Keyboard {
+  constructor() {
+    const a_z = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    this.letters = [];
+    for (const letter of a_z) {
+      this.letters.push({
+        letter,
+        state: 'AVAILABLE',
+      });
+    }
+  }
+}
+
