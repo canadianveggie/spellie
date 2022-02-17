93a9852fa026a444fa4f268680b3ce939b113f87
Christopher Porter <crporter@gmail.com>
2022-02-17 07:55:50 -0800

    Add the ability to enforce words in the dictionary (#29)
    - Spell Checker setting added to Settings
    - Last word is always spell checked
    - Added test to ensure all target words are in the dictionary.
    - Emojis that are not in the dictionary (like ELSA, ROFL) are allowed to be guessed.
    
    Adds an extra 342KB download (minified) but we can try to optimize that later.

---
+++ test/dictionary.test.js
@@ -0,0 +1,33 @@
+const { validWords } = require("../public/dictionary.js");
+const { emojis } = require("../public/emojis");
+const { words } = require("../public/puzzles");
+
+describe("validWords", () => {
+  it("puzzles are valid words", () => {
+    const allWords = []
+      .concat(words.easy)
+      .concat(words.medium)
+      .concat(words.hard);
+    allWords.forEach((word) => {
+      expect(validWords.has(word)).toBe(true);
+    });
+  });
+  it("emojis are mostly valid words", () => {
+    const valid = [];
+    const invalid = [];
+    Object.keys(emojis).forEach((emojiWord) => {
+      if (validWords.has(emojiWord)) {
+        valid.push(emojiWord);
+      } else {
+        invalid.push(emojiWord);
+      }
+    });
+    // console.log({ valid: valid.length, invalid: invalid.length });
+    // Was { valid: 419, invalid: 66 }
+    // console.log(invalid);
+    // Invalid includes ABCD, ELSA, OLAF, TADA,  and country names
+    expect(invalid.length).toBeGreaterThan(0);
+    expect(valid.length).toBeGreaterThan(invalid.length);
+    expect(invalid.length).toBeLessThan(valid.length / 5); // Less than 20% are invalid
+  });
+});
