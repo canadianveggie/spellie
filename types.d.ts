189546a1fcfc0bbc3854c909b3353e305b3f1c6e
David Hirtle <wachunga@gmail.com>
2022-02-19 19:08:02 -0800

    add some types to make dev easier

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+export type Hint = {
+  /** to display to user */
+  text: string;
+  /** to highlight on the keyboard */
+  letter?: string;
+};
+
+export type KeyState = {
+  /** most keys are letters but one is backspace, one is to submit a guess */
+  type: "letter" | "back" | "enter";
+  /** shown on the keyboard */
+  label: string;
+  /** keys start as "available" and update as user guesses (based on presence in the target word) */
+  state: "available" | "match" | "present" | "miss";
+};
