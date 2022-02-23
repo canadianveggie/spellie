40aa5ca027c270c334b62f21d9cfef1b1e6fce69
David Hirtle <wachunga@gmail.com>
2022-02-22 22:33:09 -0800

    add tests

--- test/emojis.test.js
+++ test/emojis.test.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { emojis } = require("../public/emojis");
+const { emojis, getEmojiImage } = require("../public/emojis");
 
 describe("emojis", () => {
   it("uppercase keys", () => {
@@ -39,5 +39,24 @@ describe("emojis", () => {
       }
       return memo;
     }, {});
+  });
+});
+
+describe("getEmojiImage", () => {
+  it("unicode 13 character", () => {
+    const src = getEmojiImage("🪰");
+    expect(src).toBe(`https://twemoji.maxcdn.com/v/13.1.0/svg/1fab0.svg`);
+  });
+  it("variants", () => {
+    const src = getEmojiImage("👩‍⚕️");
+    expect(src).toBe(
+      `https://twemoji.maxcdn.com/v/13.1.0/svg/1f469-200d-2695-fe0f.svg`
+    );
+  });
+  it("trailing zero width joiner", () => {
+    const src1 = getEmojiImage("⭐️");
+    const src2 = getEmojiImage("\u2B50\uFE0F");
+    expect(src1).toBe(`https://twemoji.maxcdn.com/v/13.1.0/svg/2b50.svg`);
+    expect(src2).toEqual(src1);
   });
 });
