e42c1a935094e8fd0ded47e87582d695dd393b2a
David Hirtle <wachunga@gmail.com>
2022-03-02 21:10:20 -0800

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
