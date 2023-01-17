eb614391e737d4b4c2a38dcf9875be4f1c154838
Chris Porter <chris.porter@uplight.com>
2023-01-16 16:20:56 -0800

    Download emoji file in script

--- scripts/emojicheck.js
+++ scripts/emojicheck.js
@@ -1,5 +1,8 @@
 // @ts-check
 
+const https = require("https");
+const { emojis } = require("../public/emojis");
+
 // constraints:
 // - 4-6 letter words
 // - prefer shorter where possible
@@ -12,58 +15,80 @@
 // TODO: should we allow multiple ways to unlock emoji?
 // eg "⚾️" could be PITCH, BUNT, CATCH, etc
 // also DOLL, DOLLS
+const url =
+  "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json";
 
-const { emojis } = require("../public/emojis");
-// get from https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json
+https
+  .get(url, (res) => {
+    let jsonString = "";
+    res.on("data", (data) => {
+      jsonString += data;
+    });
 
-/** @type {{emoji: string, description: string, category: string, aliases: string[],
- *          tags: string[], unicode_version: string, ios_version: string}[]} */
-const emojiJson = require("./emoji.json");
+    res.on("end", function () {
+      if (res.statusCode === 200) {
+        try {
+          /**
+           * @type {{emoji: string, description: string, category: string, aliases: string[],
+           *         tags: string[], unicode_version: string, ios_version: string}[]}
+           */
+          const emojiJson = JSON.parse(jsonString);
 
-const takenEmojis = Object.values(emojis);
-const takenKeys = Object.keys(emojis).map((key) => key.toLowerCase());
+          const takenEmojis = Object.values(emojis);
+          const takenKeys = Object.keys(emojis).map((key) => key.toLowerCase());
 
-// too new
-// TODO: allow 13 now that we're using twemojis
-const versionBlockList = ["13.0", "13.1", "13.2", "14.0"];
+          // too new
+          // TODO: allow 13 now that we're using twemojis
+          const versionBlockList = ["13.0", "13.1", "13.2", "14.0"];
 
-const categoryBlockList = [
-  "Smileys & Emotion",
-  "Animals & Nature",
-  "Travel & Places",
-  // below have been thoroughly reviewed for version < 13:
-  "Activities",
-  "Flags",
-  "Food & Drink",
-  "Objects",
-  "People & Body",
-  "Symbols",
-];
+          const categoryBlockList = [
+            "Smileys & Emotion",
+            "Animals & Nature",
+            "Travel & Places",
+            // below have been thoroughly reviewed for version < 13:
+            "Activities",
+            "Flags",
+            "Food & Drink",
+            "Objects",
+            "People & Body",
+            "Symbols",
+          ];
 
-const remaining = emojiJson
-  .filter((item) => !versionBlockList.includes(item.ios_version))
-  .filter((item) => !categoryBlockList.includes(item.category))
-  .filter((item) => !takenEmojis.includes(item.emoji))
-  .filter((item) => {
-    const keywords = item.aliases.concat(item.tags);
-    return !takenKeys.find((taken) => keywords.includes(taken));
+          const remaining = emojiJson
+            .filter((item) => !versionBlockList.includes(item.ios_version))
+            .filter((item) => !categoryBlockList.includes(item.category))
+            .filter((item) => !takenEmojis.includes(item.emoji))
+            .filter((item) => {
+              const keywords = item.aliases.concat(item.tags);
+              return !takenKeys.find((taken) => keywords.includes(taken));
+            });
+          console.log(JSON.stringify(remaining, null, 2));
+
+          const suggestions = remaining.reduce(
+            /**
+             * @param {{[word: string]: string}} mapping
+             */
+            (mapping, item) => {
+              item.aliases
+                .concat(item.tags)
+                .filter((word) => word.length >= 4 && word.length <= 6)
+                .forEach((word) => {
+                  mapping[word] = item.emoji;
+                });
+              return mapping;
+            },
+            {}
+          );
+          console.log(suggestions);
+          console.log(remaining.length);
+        } catch (e) {
+          console.error(`Error parsing emoji JSON {e}`);
+        }
+      } else {
+        console.error(`Error downloding emojis from {url} - {res.statusCode}`);
+      }
+    });
+  })
+  .on("error", (e) => {
+    console.error(e);
   });
-console.log(JSON.stringify(remaining, null, 2));
-
-const suggestions = remaining.reduce(
-  /**
-   * @param {{[word: string]: string}} mapping
-   */
-  (mapping, item) => {
-    item.aliases
-      .concat(item.tags)
-      .filter((word) => word.length >= 4 && word.length <= 6)
-      .forEach((word) => {
-        mapping[word] = item.emoji;
-      });
-    return mapping;
-  },
-  {}
-);
-console.log(suggestions);
-console.log(remaining.length);
